3ff626d1830f7bdbfd0167e7a9616267d4bba1d1
W1llbedone <n.tipper05@gmail.com>
2018-08-03 22:10:32 -0600

    updated and is fully functional. Ready for testing and review.

--- Nathan/testNT/test.js
+++ Nathan/testNT/test.js
@@ -1,3 +1,13 @@
+// data = 
+// { 
+//     "question" : "What year did Japan surrender in World War II?",
+//     "answer" : [1, 2],
+//     "options" : ["Yo", "No"],
+//     "explaination" : "Bombs"
+// }
+
+// console.log(nt.determine_type(data));
+
 nt.register("test", "test-solutions", "test.json", function() {
     nt.create_exercises("test");
 })
