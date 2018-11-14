4f56093226ae29925b4b0bb1ae020be62552a90a
W1llbedone <n.tipper05@gmail.com>
2018-11-14 15:35:43 -0700

    completed drawing of air traffic on map.

---
+++ Nathan/codingNotebook/config.js
@@ -0,0 +1,3 @@
+var AT_INFO = object.freeze({ COUNTRY: 0, TIME: 1, LAST_CONTACT: 2, POSITION: 3, ON_GROUND: 4 });
+
+var countryToShow = "Spain,United States";
--- Nathan/codingNotebook/geomapTest.js
+++ Nathan/codingNotebook/geomapTest.js
@@ -1,61 +1,106 @@
-let width = 1280,
-    height = 1000;
+require.config({
+  paths: {
+    d3: "https://d3js.org/d3.v4.min",
+    d3geo: "./d3-geo",
+    array: "./d3-array",
+    config: "./config"
+  }
+});
+require(['d3', 'd3geo', 'array', 'config'], function(d3) {
+    // define the width and height of the svg
+    let width = 1280,
+        height = 1000;
 
-let canadaSVG = d3.select("#map")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    // create the svg using d3.js
+    let canadaSVG = d3.select("#map")
+        .append("svg")
+          .attr("width", width)
+          .attr("height", height);
 
-var scalar = 200;
+    // append a grouping of drawings to the svg
+    var g = canadaSVG.append("g");
 
-var projection = d3.geoMercator()
-                    .scale([scalar])
-                    .center([-45.5017, 73.5673])
+    // The scale at which we see the map. Modify if you wish the initial size to be smaller or larger.
+    var scalar = 300;
 
-projection.translate([ (width / 2) + 100, (height / 2) - 100]);
+    // Create zoom functionality.
+    var zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
 
-var geoGenerator = d3.geoPath()
-  .projection(projection);
+    canadaSVG.call(zoom);
 
-var tooltip = d3.select("body")
-                  .append("div")
-                  .attr("class", "tooltip")
-                  .style("opacity", 0);
+    // Define the projection to use for the map of Canada
+    var projection = d3.geoMercator()
+                        .scale([scalar])
+                        .center([-45.5017, 73.5673])
 
-d3.json("canada.json", function(error, canada) {
-    if(error) return console.error(error);
+    // Translate the projection to the "center"
+    projection.translate([ (width / 2) + 100, (height / 2) - 100]);
 
-    canadaSVG.selectAll("path")
-      .data(canada.features)
-          .enter()
-            .append("path")
-              .attr("d", geoGenerator);
+    // Push the projection we used to the generator to generate the paths needed
+    var geoGenerator = d3.geoPath()
+      .projection(projection);
 
-    data = [{lat: 45.5016998, lon: -73.5672559999999 }, {lat: 46.829853, lon: -71.253028 }];
-    canadaSVG.selectAll("circle")
-      .data(data)
-        .enter()
-          .append("circle")
-            .attr("cx", function(d) {
-              return projection([d.lon, d.lat])[0];
-            })
-            .attr("cy", function(d) {
-              return projection([d.lon, d.lat])[1];
-            })
-            .attr("r", 3)
-            .style("fill", "orange")
-            .on("mouseover", function(d) {
-                tooltip.transition()
-                          .duration(200)
-                          .style("opacity", 0.9);
+    // Create a tooltip which will show when hovering on a particular plane
+    var tooltip = d3.select("body")
+                      .append("div")
+                      .attr("class", "tooltip")
+                      .style("opacity", 0);
 
-                tooltip.style("left", (d3.event.pageX) + "px")
-                        .style("top", (d3.event.pageY) + "px")
-                        .text("Hello");
-            })
-            .on("mouseout", function(d) {
-              tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-            })
+    // Create the map using the json file provided
+    d3.json("canada.json", function(error, canada) {
+        if(error) return console.error(error);
+
+        // Draw the paths which make up Canada using the json data
+        g.selectAll("path")
+          .data(canada.features)
+              .enter()
+                .append("path")
+                  .attr("d", geoGenerator);
+
+        // Now, draw the plans on the map of Canada using the air traffic data
+        d3.json("air-traffic.json", function(error, airTraffic) {
+          if(error) return console.error(error);
+
+          g.selectAll("circle")
+            .data(airTraffic.states)
+              .enter()
+                .append("circle")
+                  .attr("cx", function(d) {
+                    return projection([ d[5], d[6] ])[0];
+                  })
+                  .attr("cy", function(d) {
+                    return projection([ d[5], d[6] ])[1];
+                  })
+                  .attr("r", 2)
+                  .style("fill", "red")
+                  .on("mouseover", mouseOverHandler)
+                  .on("mouseout", mouseOutHandler);
+
+          // Now we can filter some data using the configuration file that you edited.
+          g.selectAll("circle").filter(function(d) {
+            if(!d[8]) {
+              return true;
+            }
+          })
+          .style("fill", "green")
+          .attr("r", 5);
+        });
+    });
+
+    // The zoom function to allow use to zoom in and out of the map.
+    function zoomed() {
+      g.attr("transform", d3.event.transform);
+    }
+
+    // mouseOverHandler is the functionality which makes the tooltip appear over a place.
+    function mouseOverHandler(d) {
+      tooltip.style(left)
+    }
+
+    // mouseOutHandler makes the tooltip disappear when you hover outside of a plane.
+    function mouseOutHandler(d) {
+
+    } 
 });
